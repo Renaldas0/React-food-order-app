27b64fd4adc7fca450250aa5272ebf377ad1e4f4
Renaldas Bendikas <97538312+Renaldas0@users.noreply.github.com>
2024-02-08 16:22:22 +0000

    add modal for cart functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,19 @@ import { useContext } from 'react';
 
 import logo from '../assets/logo.jpg';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext)
 
     const totalCartItems = cartCtx.items.reduce((totalItems, item) => {
         return totalItems + item.quantity;
     }, 0)
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return (
         <header id="main-header">
@@ -18,7 +24,7 @@ export default function Header() {
                 <h1>HomeRecipes</h1>
             </div>
             <nav>
-                <Button textOnly>Cart ({totalCartItems})</Button>
+                <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
